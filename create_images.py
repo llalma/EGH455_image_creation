849c1b70c9148f84a26471977ad229c88fc9145e
llalma <lhulsmanbenson@gmail.com>
2020-08-16 16:29:06 +1000

    can add multiple of the detection saign to be labeleld the samew thing

--- create_images.py
+++ create_images.py
@@ -1,4 +1,4 @@
-from PIL import Image
+from PIL import Image, ImageEnhance
 import PIL
 import random
 from math import cos,sin
@@ -9,6 +9,8 @@ from os.path import isfile, join
 import glob, os
 import numpy as np
 import cv2
+import cv2.aruco as aruco
+from time import sleep
 
 
 class Coords: 
@@ -34,26 +36,31 @@ class Coords:
 #end
 
 def load_images(background_size,signs_size):
-    #Load base images
-    # signs = ["chems.png","aruco.png","goods.png",'sign1.png','sign2.png','sign3.png','sign4.png','sign5.png','sign6.png','sign7.png','sign8.png','sign9.png']
-    signs = [f for f in listdir('images/') if isfile(join('images/', f))]
-    # background_size = (3000,3000)
-    # signs_size = (300,300)
 
-    
-    signs_img = {}
-    for i,sign in enumerate(signs):
-        signs_img[str(i)] = Image.open('images/'+sign).resize(signs_size).convert('RGBA')
+    #Signs to Detect
+    detect_signs = ["0-1.png","0-2.png","1-1.png","1-2.png","2-1.png","2-2.png",]
+    real_signs = {}
+    for i,sign in enumerate(detect_signs):
+        real_signs[detect_signs[i][0:3]] = Image.open('images/'+sign).resize(signs_size).convert('RGBA')
     #end
 
-    # backgrounds = ['floor1.png','floor2.png','floor3.png','floor4.png','floor5.png','floor6.png','floor7.png']
+    #Fake signs
+    fakes = ["sign1.png","sign3.png","sign5.png","sign6.png"]
+    fake_signs = {}
+    for i,sign in enumerate(fakes):
+        fake_signs["Fake"+str(i)] = Image.open('images/'+sign).resize(signs_size).convert('RGBA')
+    #end
+
+
+
+    #Load backgrounds
     backgrounds = [f for f in listdir('backgrounds/') if isfile(join('backgrounds/', f))]
     output_backgrounds = []
     for background in backgrounds:
         output_backgrounds.append(Image.open('backgrounds/'+background).resize(background_size).convert('RGBA'))
     #end
 
-    return output_backgrounds, signs_img
+    return output_backgrounds, real_signs, fake_signs
 #end
 
 def section(img,sq_size):
@@ -90,9 +97,9 @@ def text_for_creation(f,img_num,width,height):
 def save_to_text_file(f,label,x_pos,y_pos,width,height):
     #Save the super imposed images coordinates into a text file that yolo can use.
     
-    labels = {"1":"Chemical_Sign",
-                "0":"Aruco",
-                "2":"Dangerous_Goods"}
+    labels = {"0":"Aruco",
+                "1":"Dangerous_Goods",
+                "2":"Chemical_Sign"}
 
     write_text = """\n  <object>
     <name>{0}</name>
@@ -181,7 +188,35 @@ def rotate(img):
     return img.rotate(random.randint(0,360))
 #end
 
-def superImpose(background, imgs, labels,sq_size,labels_list,i):
+def contrast(img):
+    enhancer = ImageEnhance.Contrast(img)
+
+    factor = random.randint(70,130)/100
+    im_output = enhancer.enhance(factor)
+
+    return im_output
+#end
+
+def get_image(label,imgs):
+    if label[0] == "0":
+        #Random aruco
+        random_aruco_index = random.randint(0,249)
+        #Non changing aruco
+        # random_aruco_index = 0
+
+        aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_250)
+        # second parameter is id number
+        # last parameter is total image size
+        img = aruco.drawMarker(aruco_dict, random_aruco_index, 100)
+        img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGBA)
+        img = Image.fromarray(img)
+    else:
+        img = imgs.get(str(label))
+    #end
+    return img
+#end
+
+def superImpose(background,real,fake, labels,sq_size,i):
     #Superimposes all images in imgs ontop of background with no overlap, then saves the image into images folder.
     save_name = str(i)
     #Label file
@@ -196,13 +231,23 @@ def superImpose(background, imgs, labels,sq_size,labels_list,i):
     previous_pasted_coords = []
 
     for label in labels:
+        #Check if a bounding box needs to be drawn for the sign
+        bounding_box = False
+        if label[0] in ["0","1","2"]:
+            #Needs a bounding box as it is a sign
+            img = get_image(label,real)
+            bounding_box = True
+        else:
+            #Just a random image doesnt need a bounding box
+            img = fake.get(label)
+        #end
 
         #Rotate image
         theta = random.randint(0,360)
-        rot = imgs.get(str(label)).rotate(theta,expand=1)
+        rot = img.rotate(theta,expand=1)
 
         #Scale Image
-        new_size = random.randint(75,rot.size[0])
+        new_size = random.randint(100,rot.size[0])
         rot.thumbnail((new_size,new_size), PIL.Image.ANTIALIAS)
 
         #Get widths and heights
@@ -214,7 +259,6 @@ def superImpose(background, imgs, labels,sq_size,labels_list,i):
 
         #If max recursion depth is reached dont include the picture
         if(x_pos < 0):
-            # print("Reach max recusion")
             break
         else:
             #Coords for bounding box, dont know why its like this but just take it.
@@ -222,8 +266,8 @@ def superImpose(background, imgs, labels,sq_size,labels_list,i):
             y1 = (y_pos+(img_height/2))/back_width
 
             #Write label text file. Only save labels which need to be detected
-            if label in labels_list:
-                save_to_text_file(f,label,x_pos,y_pos,img_width,img_height)  
+            if bounding_box:
+                save_to_text_file(f,label[0],x_pos,y_pos,img_width,img_height)  
             #end      
 
             #Superimpose image
@@ -244,6 +288,15 @@ def superImpose(background, imgs, labels,sq_size,labels_list,i):
     if i%100 == 0:
         print("Saving superimposed image and text file " + str(i))
 
+    resize_image = (640,640)
+    new_img.thumbnail(resize_image, Image.ANTIALIAS)
+    
+    #Add Gausian Noise
+    new_img = noisy(new_img)
+
+    #Change contrast
+    new_img = contrast(new_img)
+
     new_img.convert('RGB').save('generated_images//images//'+save_name+'.jpg')
 #end
 
@@ -262,35 +315,34 @@ files = glob.glob('generated_images/images/*')
 for f in files:
     os.remove(f)
 
-files = glob.glob('generated_images/labels/*')
-for f in files:
-    os.remove(f)
-
 #############################################################################################################################
 #Change these lines to adjust the resolution of the model and the generated size
-background_size = (1000,1000)   #Inital background load in size, want this to be a high res image so when sectioned it will still look good.
+background_size = (640,640)   #Inital background load in size, want this to be a high res image so when sectioned it will still look good.
 overlay_size = (100,100)    #Image which is overlayed will be a random size between 100 and this size. This needs to be smaller than generated_size
 generated_size = 640   #Output size of the image, specificies how big a section is from the background.
 
-num_images_to_genereate = 500  #Number of images to generate
+num_images_to_genereate = 4000  #Number of images to generate
 
-labels_list = [0,1,2]   #Add more labels as needed. Need to make sure that the overlayed images are in order at the top of the images folder. so the first 3 by alphabetical will be the labelled ones.
 #############################################################################################################################
 
 
 #Load in 
-floors, signs = load_images(background_size,overlay_size)
+floors, real_signs, fake_signs = load_images(background_size,overlay_size)
 
 for i in range(num_images_to_genereate):
-    #A random number of signs to impose between 1 and 5.
-    #Then randomly selects between the 3 different signs
-    num_to_impose = 5
+    num_to_impose = 3
     signs_to_impose = []
-    for n in range(0,random.randint(1,num_to_impose)):
-        signs_to_impose.append(random.randint(0,len(signs)-1))
+
+    real_impose = range(0,random.randint(1,num_to_impose))
+    for n in real_impose:
+        signs_to_impose.append(random.choice(list(real_signs.keys())))
+    #end
+
+    fake_impose = range(0,random.randint(1,num_to_impose))
+    for n in fake_impose:
+        signs_to_impose.append(random.choice(list(fake_signs.keys())))
     #end
 
     #Superimposes image, saves output and bounding box in YOLO format.
-    superImpose(random.choice(floors),signs,signs_to_impose,generated_size,labels_list,i)
+    superImpose(random.choice(floors),real_signs,fake_signs,signs_to_impose,generated_size,i)
 #end
-
